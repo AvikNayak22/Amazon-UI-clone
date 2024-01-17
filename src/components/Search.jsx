b55c16f95bd0f5d603b2436b7f2523342669ed54
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2024-01-17 20:26:14 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,23 @@
+// Import necessary components and functions from external libraries and modules
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useState, useEffect } from "react";
 import { useNavigate, createSearchParams } from "react-router-dom";
 
 import { callAPI } from "../utils/CallApi";
 
+// Define the Search component
 const Search = () => {
+  // State variables to manage suggestions, search term, and selected category
   const [suggestions, setSuggestions] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [category, setCategory] = useState("All");
   const navigate = useNavigate();
 
+  // Function to handle form submission
   const onHandleSubmit = (e) => {
     e.preventDefault();
 
+    // Navigate to the search page with category and search term parameters
     navigate({
       pathname: "search",
       search: `${createSearchParams({
@@ -21,23 +26,28 @@ const Search = () => {
       })}`,
     });
 
+    // Reset search term and category
     setSearchTerm("");
     setCategory("All");
   };
 
+  // Function to fetch search suggestions from an API
   const getSuggestions = () => {
     callAPI(`data/suggestions.json`).then((suggestionResults) => {
       setSuggestions(suggestionResults);
     });
   };
 
+  // Fetch suggestions when the component mounts
   useEffect(() => {
     getSuggestions();
   }, []);
 
+  // Render the Search component
   return (
     <div className="w-[100%]">
       <div className="flex items-center h-10 bg-amazonclone-yellow rounded">
+        {/* Dropdown to select category */}
         <select
           onChange={(e) => setCategory(e.target.value)}
           className="p-2 bg-gray-300 text-black border text-xs xl:text-sm"
@@ -50,16 +60,19 @@ const Search = () => {
           <option>Home</option>
           <option>Mobiles</option>
         </select>
+        {/* Input field for search term */}
         <input
           className="flex grow items-center h-[100%] rounded-l text-black"
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
+        {/* Button to submit the search */}
         <button onClick={onHandleSubmit} className="w-[45px]">
           <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900" />
         </button>
       </div>
+      {/* Display search suggestions if available */}
       {suggestions && (
         <div className="bg-white text-black w-full z-40 absolute">
           {suggestions
@@ -74,6 +87,7 @@ const Search = () => {
             })
             .slice(0, 10)
             .map((suggestion) => (
+              // Render each suggestion as a clickable item
               <div
                 key={suggestion.id}
                 onClick={() => setSearchTerm(suggestion.title)}
@@ -87,4 +101,6 @@ const Search = () => {
   );
 };
 
+// Export the Search component for use in other parts of the application
 export default Search;
+
